59c0b2a72bdd6e5caf449b0cdf4a013ecb72502f
Twilight Sparkle <19155609+Twi1ightSparkle@users.noreply.github.com>
2020-04-01 12:10:56 +0100

    Cleanup and comments

--- process_shodan.py
+++ process_shodan.py
@@ -1,6 +1,5 @@
 import json
 import os
-import pycurl
 import random
 import re
 import subprocess
@@ -8,32 +7,38 @@ import sys
 import requests
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
-from timeit import default_timer
 
 try:
     from io import BytesIO
 except ImportError:
     from StringIO import StringIO as BytesIO
 
-START_TIME = default_timer()
 
+# Paths
 workDir = os.path.dirname(os.path.realpath(__file__))
 shodanFile = open(os.path.join(workDir, 'shodan-export-port-8448.json'))
 
 
-# f = open(os.path.join(workDir, 'html', 'synapses.js'), 'w+')
-# f.write('var addressPoints = [\n')
-
+# Split into workers. Assuming less than or slightly more than 28k entries
 line_index = int(sys.argv[1])
 lines = [line for line in shodanFile]
 if line_index > 6:
     lines = lines[line_index * 3500 :]
 else:
     lines = lines[line_index * 3500 : (line_index + 1) * 3500]
-random.shuffle(lines)
-random.shuffle(lines)
+random.shuffle(lines) # Randomize the list
+
 
 def curl(line):
+    '''
+    Do curl stuff.
+
+    Args:
+        line: a text string
+
+    Returns
+        json response
+    '''
     data = json.loads(line)
     if data['transport'] == 'tcp':
         lon = data['location']['longitude']
@@ -55,9 +60,6 @@ def curl(line):
 
 
 async def get_data_asynchronous():
-    f = open(os.path.join(workDir, 'html', 'synapses.js'), 'w+')
-    f.write('var addressPoints = [\n')
-
     with ThreadPoolExecutor(max_workers=10) as executor:
         # Set any session parameters here before calling `fetch`
         loop = asyncio.get_event_loop()
@@ -72,9 +74,6 @@ async def get_data_asynchronous():
         for response in await asyncio.gather(*tasks):
             if response:
                 print(response)
-
-    f.write('];')
-    f.close()
 
 
 def main():
