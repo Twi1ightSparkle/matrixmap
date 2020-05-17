e71e29932f32e1c1f546898cce182570066507bf
Twilight Sparkle <19155609+Twi1ightSparkle@users.noreply.github.com>
2020-05-17 19:19:13 +0100

    Moved a bunch of functions to own files

--- process_homeservers.py
+++ process_homeservers.py
@@ -1,31 +1,13 @@
-## Import stuff
-
-import aiofiles
+## Import modules
 import asyncio
-import dns
-import json
-import logging
+import configparser
 import os
-import psycopg2
 import random
-import re
-import requests
-import ssl
-import socket
-import srvlookup
-import subprocess
-import sys
-import urllib3
 from concurrent.futures import ThreadPoolExecutor
-from fake_useragent import UserAgent
 
-try:
-    from io import BytesIO
-except ImportError:
-    from StringIO import StringIO as BytesIO
-
-## Global variables
-requests_timeout = 1
+## Import other python files
+from util import delegated_hostname
+from util import resolve_server
 
 
 ## Functions
@@ -47,329 +29,15 @@ def file_len(f_name):
     return i + 1
 
 
-def resolve_well_known(hostname):
-    """Get delegated hostname and port from hostname
-
-    Try and look up the well-know server file for a hostname, then if this file exists return
-    the delegated hostname and port, or return an empty string if no well known server file exists.
-
-    Args:
-        hostname: A hostname as found in the Matrix ID
-    
-    Returns:
-        Delegated hostname and port in the format sub.domain.tld:port:wellknown
-        Or an empty string
-    """
-
-    # Set a random valid user-agent
-    ua = UserAgent()
-    headers = {'User-Agent': ua.random}
-
-    # Set well-known URL
-    well_known_url = f'https://{hostname}/.well-known/matrix/server'
-
-    # Try to downlad well-known server file
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    try:
-        well_known_request = requests.get(well_known_url, headers=headers, allow_redirects=True, verify=False, timeout=requests_timeout)
-    except (
-        dns.name.LabelTooLong,
-        NameError,
-        requests.exceptions.ConnectionError,
-        requests.exceptions.ReadTimeout,
-        requests.exceptions.SSLError,
-        requests.exceptions.TooManyRedirects,
-        socket.timeout,
-        ssl.SSLCertVerificationError,
-        ssl.SSLError,
-        UnicodeError,
-        requests.exceptions.ConnectionError,
-        urllib3.exceptions.MaxRetryError,
-        urllib3.exceptions.NewConnectionError
-
-    ):
-        return(None)
-
-    # If not 200
-    if not well_known_request.status_code == 200:
-        return(None)
-
-    # Try and decode json, then split domain.tld:port
-    try:
-        delegated_hostname, delegated_port = str(well_known_request.json()['m.server']).split(':')
-
-    # If converting to json fails it's probably a bitstream or something
-    except json.decoder.JSONDecodeError:
-        return(None)
-    
-    except ValueError:
-        delegated_port = 443
-    
-    else: 
-        return(f'{delegated_hostname}:{delegated_port}:wellknown')
-
-
-def resolve_srv(hostname):
-    """Get delegated hostname and port from DNS SRV record
-
-    Try and look up the DNS SRV record for a hostname, then if this exists return
-    the delegated hostname, port and srv, or return an empty string if no well known server file exists.
-
-    Args:
-        hostname: A hostname as found in the Matrix ID
-    
-    Returns:
-        Delegated hostname and port in the format sub.domain.tld:port:srv
-        Or an empty string
-    """
-
-    # Turn off annoying logging to terminal from srv lookup
-    logger = logging.getLogger('srvlookup').setLevel(logging.CRITICAL)
-    logger = logger
-    
-    # Try and look up SRV record
-    try:
-        srv = srvlookup.lookup('matrix', 'TCP', hostname)
-
-    # SRV lookup fail (except no record found)
-    except (srvlookup.SRVQueryFailure, UnicodeError, dns.name.LabelTooLong):
-        return(None)
-    
-    # SRV lookup returns something
-    else:
-        # Successful SRV lookup
-        if 'Error querying SRV' not in srv[0]:
-            delegated_hostname = srv[0].hostname
-            delegated_port = srv[0].port
-            return(f'{delegated_hostname}:{delegated_port}:srv')
-        
-        # Error quering
-        else:
-            return(None)
-
-
-def resolve_delegated_homeserver(hostname):
-    """Return delegated hostname and port from a hostname
-
-    Tries to looks up well-known server file, then SRV DNS record.
-    If both fail, return the arg hostname with assumed port 8448
-
-    Args:
-        hostname: hostname (the domain part of a MAtrix ID)
-
-    Returns:
-        A string containing delegated hostname and port for the hostname in this format:
-        sub.domain.tld:port:server-resolve-type
-    """
-
-    # If hostname is an ip
-    pattern = re.compile(r'\d+\.\d+\.\d+\.\d')
-    if pattern.match(hostname):
-        return(f'{hostname}:8448:ip')
-
-    # If a well-known
-    temp = resolve_well_known(hostname)
-    if temp:
-        return temp
-    
-    # If a srv
-    temp = resolve_srv(hostname)
-    if temp:
-        return temp
-    
-    # Else, assume A or AAAA and assume port 8448
-    return(f'{hostname}:8448:a')
-
-
-def https_download(hostname, path, port=443):
-    """Try and download something over https
-
-    Try and download something from https. Decode and return whatever it downloaded or return empty of download failed
-
-    Args:
-        hostname: A hostname or an IP address
-        path: What do download. For example /_matrix/static
-        port: A port, default 443
-    
-    Returns:
-        Some decoded content if there is some or not 404. If 404 or otherwise fail, return empty string
-    """
-
-    # Set a random valid user-agent
-    ua = UserAgent()
-    headers = {'User-Agent': ua.random}
-
-    # Set  URL
-    url = f'https://{hostname}:{port}{path}'
-
-    # Try and download
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    try:
-        http_request = requests.get(url, headers=headers, allow_redirects=True, verify=False, timeout=requests_timeout)
-    except (
-        dns.name.LabelTooLong,
-        NameError,
-        requests.exceptions.ConnectionError,
-        requests.exceptions.ReadTimeout,
-        requests.exceptions.SSLError,
-        requests.exceptions.TooManyRedirects,
-        socket.timeout,
-        ssl.SSLCertVerificationError,
-        ssl.SSLError,
-        UnicodeError,
-        requests.exceptions.ConnectionError,
-        urllib3.exceptions.MaxRetryError,
-        urllib3.exceptions.NewConnectionError
-
-    ):
-        return(None)
-
-    # If not 200
-    if not http_request.status_code == 200:
-        return(None)
-
-    # Try and decode json, then split domain.tld:port
-    try:
-        delegated_hostname, delegated_port = str(http_request.json()['m.server']).split(':')
-
-    # If converting to json fails it's probably a bitstream or something
-    except json.decoder.JSONDecodeError:
-        return(None)
-
-    # Split on : fail due to no : being present, assume port 443
-    except ValueError:
-        return(f'{delegated_hostname}:443')
-    
-    else: 
-        return(f'{delegated_hostname}:{delegated_port}')
-
-
-def check_matrix_server(hostname):
-    """Check if and save there is a Synapse or Dendrite server on a url
-
-    Check if there is a Synapse or Dendrite server on a url:port. If there is a Synapse/Dendrite there,
-    look up IP and version, then store information to database
-
-    Args:
-        hostname: Some URL from Matrix IDs in format sub.domain.com
-    
-    Return:
-        TODO
-    """
-    
-    # Clean up hostname to exclude errors
-    hostname = hostname.strip()
-    hostname = hostname.replace('http://', '')
-    hostname = hostname.replace('https://', '')
-    if '!' in hostname:
-        hostname = hostname.replace('!', '')
-        x, hostname = hostname.split(':', 1)
-    if '?' in hostname:
-        hostname, x = hostname.split('?', 1)
-
-    # If port is already known
-    port = None
-    if ':' in hostname:
-        hostname, port = hostname.split(':')
-
-    # Get delegated stuff
-    delegated_hostname, delegated_port, server_lookup_type = str(resolve_delegated_homeserver(hostname)).split(':')
-
-    if port:
-        delegated_port = port
-
-    # Set a random valid user-agent
-    ua = UserAgent()
-    headers = {'User-Agent': ua.random}
-
-    # Set version URL
-    version_url = f'https://{delegated_hostname}:{delegated_port}/_matrix/federation/v1/version'
-    
-    # Try to downlad version
-    valid_ssl = True
-    try:
-        version_request = requests.get(version_url, headers=headers, allow_redirects=True, timeout=requests_timeout)
-    
-    # If ssl error
-    except (requests.exceptions.SSLError, ssl.SSLCertVerificationError, ssl.SSLError):
-        try:
-            version_request = requests.get(version_url, headers=headers, allow_redirects=True, verify=False, timeout=requests_timeout)
-        except (
-            dns.name.LabelTooLong,
-            NameError,
-            requests.exceptions.ConnectionError,
-            requests.exceptions.ReadTimeout,
-            requests.exceptions.SSLError,
-            requests.exceptions.TooManyRedirects,
-            socket.timeout,
-            ssl.SSLCertVerificationError,
-            ssl.SSLError,
-            UnicodeError,
-            requests.exceptions.ConnectionError,
-            urllib3.exceptions.MaxRetryError,
-            urllib3.exceptions.NewConnectionError
-        ):
-            pass
-        valid_ssl = False
-    
-    # If connection error
-    except (
-        dns.name.LabelTooLong,
-        NameError,
-        requests.exceptions.ConnectionError,
-        requests.exceptions.ReadTimeout,
-        requests.exceptions.TooManyRedirects,
-        socket.timeout,
-        UnicodeError,
-        requests.exceptions.ConnectionError,
-        urllib3.exceptions.MaxRetryError,
-        urllib3.exceptions.NewConnectionError
-
-    ):
-        return(None)
-
-    # If not response code 200
-    try:
-        if not version_request.status_code == 200:
-            return(None)
-    except:
-        return(None)
-
-    
-    # Try and decode json
-    try:
-        version_json = version_request.json()
-
-    # If converting to json fails it's probably a bitstream or something
-    except json.decoder.JSONDecodeError:
-        return(None)
-    
-    # Get version data
-    else:
-        name = version_json['server']['name']
-        version = version_json['server']['version']
-    
-    # Get the IP for the Matrix server
-    delegated_ip = socket.gethostbyname(delegated_hostname)
-    
-    # Create ; separated string and return it
-    out_string = f'{hostname};{delegated_hostname};{delegated_ip};{delegated_port};{server_lookup_type};{name};{version};'
-    if valid_ssl:
-        out_string += 'yes'
-    else:
-        out_string += 'no'
-    return(out_string)
-
-
-async def get_data_asynchronous():
-    with ThreadPoolExecutor(max_workers=50) as executor:
-        # Set any session parameters here before calling `fetch`
+async def get_data_asynchronous(workers):
+    with ThreadPoolExecutor(max_workers=workers) as executor:
+        # Set any session parameters here before calling check_matrix_server
         loop = asyncio.get_event_loop()
         tasks = [
             loop.run_in_executor(
                 executor,
-                check_matrix_server,
-                hostname # Allows us to pass in multiple arguments to `fetch`
+                resolve_server.check_matrix_server,
+                hostname # Allows us to pass in multiple arguments
             )
             for hostname in hostnames
         ]
@@ -379,10 +47,83 @@ async def get_data_asynchronous():
 
 
 if __name__ == "__main__":
-    # Make nice and short variables for paths
-    workDir = os.path.dirname(os.path.realpath(__file__))
-    hostnames_file = open(os.path.join(workDir, 'homeservers.txt'))
-    out_file = os.path.join(workDir, 'info.csv')
+    # Load config
+    work_dir = os.path.dirname(os.path.realpath(__file__)) # Get the path for the directory this python file is stored in
+    config = configparser.ConfigParser()
+    config.read(os.path.join(work_dir, 'config.ini'))
+
+    # Global
+    conf_global_data_directory = config.get('Global', 'data_directory')
+    
+    # PostgreSQL
+    conf_psql_enabled = config.get('PostgreSQL', 'enabled')
+    if conf_psql_enabled == 'Yes':
+        try_sql = True
+    elif conf_psql_enabled == 'No':
+        try_sql = False
+    else:
+        print('Config error. PostgreSQL: enabled must be either Yes or No')
+        exit(1)
+    conf_psql_server = config.get('PostgreSQL', 'server')
+    try:
+        conf_psql_port = int(config.get('PostgreSQL', 'port'))
+    except ValueError:
+        print('Config error. PostgreSQL: port must be an integer')
+        exit(1)
+    conf_psql_database = config.get('PostgreSQL', 'database')
+    conf_psql_username = config.get('PostgreSQL', 'username')
+    conf_psql_password = config.get('PostgreSQL', 'password')
+    try:
+        conf_psql_limit = int(config.get('PostgreSQL', 'limit'))
+    except ValueError:
+        print('Config error. PostgreSQL: limit must be an integer')
+        exit(1)
+
+    # HomeserverFile
+    conf_hs_file_filename = config.get('HomeserverFile', 'filename')
+
+    try:
+        conf_processhomeservers_workers = int(config.get('ProcessHomeservers', 'workers'))
+    except ValueError:
+        print('Config error. ProcessHomeservers: workers must be an integer')
+        exit(1)
+    conf_processhomeservers_debug = config.get('ProcessHomeservers', 'debug')
+    if conf_processhomeservers_debug == 'Yes':
+        debug = True
+    elif conf_processhomeservers_debug == 'No':
+        debug = False
+    else:
+        print('Config error. ProcessHomeservers: debug must be either Yes or No')
+        exit(1)
+
+    # Output
+    conf_output_homeservers = config.get('Output', 'homeservers')
+
+
+    # Set paths
+    hostnames_file_path = os.path.join(work_dir, conf_global_data_directory, conf_hs_file_filename)
+    out_file_path = os.path.join(work_dir, conf_global_data_directory, conf_output_homeservers)
+    
+    # Load hostnames
+    hostnames = []
+    hostnames_from_file = delegated_hostname.load_file(hostnames_file_path)
+    hostnames_from_postgres = False
+    if try_sql:
+        hostnames_from_postgres = delegated_hostname.get_hostnames_from_postgres(conf_psql_server,
+                                                                                conf_psql_port,
+                                                                                conf_psql_database,
+                                                                                conf_psql_username,
+                                                                                conf_psql_password,
+                                                                                conf_psql_limit)
+    if hostnames_from_file:
+        hostnames.extend(hostnames_from_file)
+    if hostnames_from_postgres:
+        hostnames.extend(hostnames_from_postgres)
+    
+    # Quit if no hostnames found
+    if len(hostnames) < 1:
+        print('No hostnames found, quitting')
+        exit(1)
 
     # Split into workers
     # line_index = int(sys.argv[1])
@@ -391,7 +132,6 @@ if __name__ == "__main__":
     # index = int(line_count / processes)
 
     # Split the list of hostnames into multiple sets
-    hostnames = [line for line in hostnames_file]
     # if line_index > processes - 2:
     #     hostnames = hostnames[line_index * index :]
     # else:
@@ -400,7 +140,11 @@ if __name__ == "__main__":
     # Randomize the order of the hostname list 
     random.shuffle(hostnames)
 
+    # Print headers for debug
+    if debug:
+        print('Hostname;Delegated hostname;Delegated IP;Delegated port;Server lookup type;Name;Matrix server version;Valid SSL')
+
     # Run async stuff
     loop = asyncio.get_event_loop()
-    future = asyncio.ensure_future(get_data_asynchronous())
+    future = asyncio.ensure_future(get_data_asynchronous(conf_processhomeservers_workers))
     loop.run_until_complete(future)
